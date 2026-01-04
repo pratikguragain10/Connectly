89a68722e78ff8b6143eb51141973c311b9be203
pratikguragain10 <pratikguragain69@gmail.com>
2026-01-05 01:04:04 +0530

    Connectly

--- connectly/connectly/settings.py
+++ connectly/connectly/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    "allauth.socialaccount.providers.google",
+    #"allauth.socialaccount.providers.google",
 
     "rest_framework",
     "channels",
